8dbfba58caca187eef01f326a40a3557ebb74284
odrwz <odrwz@users.noreply.github.com>
2026-04-10 22:50:09 +0800

    bump mssh to 2.1.1 with cache-busted URL

--- mssh.rb
+++ mssh.rb
@@ -1,7 +1,7 @@
 class Mssh < Formula
   desc "SSH Host & Key Manager CLI"
   homepage "https://github.com/odrwz/mssh"
-  url "https://raw.githubusercontent.com/odrwz/mssh/main/mssh"
+  url "https://raw.githubusercontent.com/odrwz/mssh/main/mssh?v=2.1.1"
   sha256 "fd3899c2f0d990f2a8e948af7785657c6e3d89283c5f43d3b31978bb45e3a955"
   version "2.1.1"
 
