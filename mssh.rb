0c0d75b7d6f3cc21a25124e7e2e3f0f110bd2919
odrwz <odrwz@users.noreply.github.com>
2026-04-10 22:27:46 +0800

    bump mssh to 2.1.1

--- mssh.rb
+++ mssh.rb
@@ -2,8 +2,8 @@ class Mssh < Formula
   desc "SSH Host & Key Manager CLI"
   homepage "https://github.com/odrwz/mssh"
   url "https://raw.githubusercontent.com/odrwz/mssh/main/mssh"
-  sha256 "d96fa87a444d6490d88e21ae6dad420d111bc869b03e8b387eb79ec8d46e596c"
-  version "2.1.0"
+  sha256 "fd3899c2f0d990f2a8e948af7785657c6e3d89283c5f43d3b31978bb45e3a955"
+  version "2.1.1"
 
   def install
     bin.install "mssh"
